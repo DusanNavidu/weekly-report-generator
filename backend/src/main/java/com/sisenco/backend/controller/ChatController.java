99887846545f722a43fdb903fa7010d2361f0441
DusanNavidu <navidu200210@gmail.com>
2026-09-07 07:01:41 +0530

    feat: add ChatController, ChatRequestDto, ChatResponseDto, and ChatService classes for chat functionality

---
+++ backend/src/main/java/com/sisenco/backend/controller/ChatController.java
@@ -0,0 +1,9 @@
+package com.sisenco.backend.controller;
+
+/**
+ * @author Dusan
+ * @date 9/7/2026
+ */
+
+public class ChatController {
+}
---
+++ backend/src/main/java/com/sisenco/backend/service/ChatService.java
@@ -0,0 +1,9 @@
+package com.sisenco.backend.service;
+
+/**
+ * @author Dusan
+ * @date 9/7/2026
+ */
+
+public class ChatService {
+}
